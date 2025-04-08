16c121f014a1152101e7bdd42223d55150947fee
Grigory Krutov <grukrutov@MacBook-Pro-Grigory.local>
2025-04-08 15:18:38 +0300

    add userController and JWT Auth

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,57 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const ApiError = require('../error/ApiError');
+const { User, Basket } = require('../models/models');
+
+const generateJWt = (id, email, role) => {
+  return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+    expiresIn: '24h',
+  });
+};
 
 class UserController {
-  async registration(req, res) {}
-  async login(req, res) {}
-  async check(req, res, next) {
-    const { id } = req.query;
-    if (!id) {
-      return next(ApiError.badRequest('Не задан id'));
+  async registration(req, res, next) {
+    const { email, password, role } = req.body;
+
+    if (!email || !password) {
+      return next(ApiError.badRequest('Некоррекнтые данные'));
     }
-    res.json(id);
+
+    const candidate = await User.findOne({ where: { email } });
+    if (candidate) {
+      return next(
+        ApiError.badRequest('Ползователь с таким email уже существует')
+      );
+    }
+
+    const hashPassword = await bcrypt.hash(password, 5);
+
+    const user = await User.create({ email, password: hashPassword, role });
+
+    const basket = await Basket.create({ userId: user.id });
+    const token = generateJWt(user.id, user.email, user.role);
+
+    return res.json({ token });
+  }
+  async login(req, res, next) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return next(ApiError.badRequest('Пользователь не найден'));
+    }
+
+    let comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) {
+      return next(ApiError.badRequest('Указан неверный пароль'));
+    }
+
+    const token = generateJWt(user.id, user.email, user.role);
+    return res.json({ token });
+  }
+  async check(req, res, next) {
+    const token = generateJWt(req.user.id, req.user.email, req.user.role);
+    return res.json({ token });
   }
 }
 
